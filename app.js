22afc8c51a40f989d8a1afcb782c8f62c8a497b8
qasmanavt <qlndrvxjbk@gmail.com>
2021-04-24 17:32:06 +0500

    Upload my changes

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    addTodo();
+})
+
+function addTodo(todo){
+    var todoText = input.value;
+
+    if(todoText){
+        const todoEl = document.createElement('li');
+        if(todo && todo.completed){
+            todoEl.classList.add('completed');
+        }
+
+        todoEl.innerText = todoText;
+
+        todosUL.appendChild(todoEl);
+
+        input.value='';
+    }
+
+    // console.log(todoText);
+}
